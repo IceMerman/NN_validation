eed4057c2efa0d7446908b9404e94af05ada35ea
IceMerman <jsjuanes51@gmail.com>
2023-05-01 15:39:15 -0500

    Changed order, and encoded valdiation type

--- Demanda/checkACLimits.m
+++ Demanda/checkACLimits.m
@@ -7,22 +7,27 @@ function [ validation ] = checkACLimits(mpc, n_shedding, options)
     qr = runpf(mpc, options);
     validation = 1;
     if qr.success
-        if any([any(abs(qr.bus(:,8)) > qr.bus(:,12)), any(abs(qr.bus(:,8)) < qr.bus(:,13))]) && 0
-            disp('The AC Power Flow voltage magnitude violation');
-            validation = 0;
-        elseif any(sqrt(qr.branch(:,14).^2 + qr.branch(:,15).^2) > qr.branch(:,8))
-            disp('The AC Power Flow transport capacity violation');
-            validation = 0;
-        elseif sum(qr.gen(end-n_shedding:end,2)) > 1e-3
-            disp('The AC Power Flow generation capacity violation');
-            validation = 0;
-        elseif sum(qr.gen(end-n_shedding:end,3)) > 1e-3
-            disp('The AC Power Flow reactive generation capacity violation');
-            validation = 0;
+        if sum(qr.gen(end-n_shedding+1:end,2)) > 1e-3
+            fprintf('\tThe AC Power Flow generation capacity violation # %d\n', validation);
+            validation = validation + 2;
+        end
+        if sum(qr.gen(end-n_shedding+1:end,3)) > 1e-3
+            fprintf('\tThe AC Power Flow reactive generation capacity violation # %d\n', validation);
+            validation = validation + 4;
+        end
+        if any(sqrt(qr.branch(:,14).^2 + qr.branch(:,15).^2) > qr.branch(:,8))
+            fprintf('\tThe AC Power Flow transport capacity violation # %d\n', validation);
+            validation = validation + 8;
+        end
+        % Para este caso siempre hay barra con subtensión
+        if any([any(abs(qr.bus(:,8)) < qr.bus(:,12)), any(abs(qr.bus(:,8)) > qr.bus(:,13))]) && 0
+            fprintf('\tThe AC Power Flow voltage magnitude violation # %d\n', validation);
+            validation = validation + 16;
         end
     else
-        validation = 0;
-        disp('The AC Power Flow of the DC solution diverges ');
+        validation = validation + 32;
+        fprintf('\tThe AC Power Flow of the DC solution diverges # %d\n', validation);
     end
+    
 end
 
